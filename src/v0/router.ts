35cc3aa8999b44f53fb8828e62133a9cc4c711fc
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-04-14 13:08:50 -0500

    refactor: use default exports

--- src/v0/router.ts
+++ src/v0/router.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { rule34Router } from "./submodule/rule34/router.ts";
+import rule34Router from "./submodule/rule34/router.ts";
 
 export const version0 = express.Router()
     .use("/rule34", rule34Router);
--- src/v0/submodule/rule34/router.ts
+++ src/v0/submodule/rule34/router.ts
@@ -1,6 +1,8 @@
 import express from "express";
 
-import { rule34SearchRoute } from "./routing/search/router";
+import searchRouter from "./routing/search/router";
 
-export const rule34Router = express.Router()
-    .get("/search", rule34SearchRoute);
+const rule34Router = express.Router()
+    .get("/search", searchRouter);
+
+export default rule34Router;
