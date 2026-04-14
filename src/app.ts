8448fa763da6b0727e3f9470f7a598ba391c4afb
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-04-14 14:49:28 -0500

    feat: add app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
+//#region router
+
 import express from "express";
 
 import { version0 } from "./v0/router.ts";
 
 const router = express.Router()
     .use("/v0", version0);
+
+//#region app
+
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+    console.log(`App listening on port ${PORT}.`);
+});
