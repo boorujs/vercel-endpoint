54dc6956767c78d1dc95caf177c6bd4da30bc48e
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-04-14 17:54:47 -0500

    fix: replace .get() with .use()

--- src/v0/submodule/rule34/router.ts
+++ src/v0/submodule/rule34/router.ts
@@ -3,6 +3,6 @@ import express from "express";
 import searchRouter from "./routing/search/router.ts";
 
 const rule34Router = express.Router()
-    .get("/search", searchRouter);
+    .use("/search", searchRouter);
 
 export default rule34Router;
