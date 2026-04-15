7101a6a85e2078d0c79c08cfcfad7fb98d015f1a
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-04-15 09:48:01 -0500

    refactor: split app and v-router files

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,7 @@
-//#region router
-
 import express from "express";
-
-import version0 from "./v0/router.ts";
-
-const versionRouter = express.Router()
-    .use("/v0", version0);
-
-//#region app
-
 import cors from "cors";
+
+import versionRouter from "./version-router.ts";
 
 const app = express();
 
