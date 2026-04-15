37275c4fd86cdd84442948095663549eb2d6c77c
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-04-15 13:04:32 -0500

    feat: add formatResults()

---
+++ src/v0/submodule/rule34/routing/search/util/format-results.ts
@@ -0,0 +1,58 @@
+import type { SimpleSearchIntersection } from "../../../types/simple-search-intersection.ts";
+
+export const formatResults = (results: SimpleSearchIntersection) => ({
+    count: results.count,
+    results: results.results.map(i => ({
+        id: i.id,
+        createdAt: i.created_at,
+        updatedAt: i.change,
+        file: {
+            main: {
+                url: i.file_url,
+                size: [ i.width, i.height ]
+            },
+            sample: {
+                has: i.sample,
+                url: i.sample_url,
+                size: [ i.sample_width, i.sample_height ]
+            },
+            preview: {
+                url: i.preview_url,
+                size: [ i.preview_width, i.preview_height ]
+            },
+            ext: i.image.match(/(?<=\.)[^.]*$/)[0],
+            md5: i.hash,
+            directory: i.directory
+        },
+        score: i.score,
+        tags: i.tag_info.reduce(
+            (obj, i) => {
+                const catMap = {
+                    "copyright": "copyright",
+                    "character": "character",
+                    "artist": "artist",
+                    "tag": "general",
+                    "metadata": "metadata",
+                    [null]: "ambiguous"
+                };
+                
+                obj[catMap[i.type]].push({ name: i.tag, count: i.count });
+            },
+            { copyright: [], character: [], artist: [],
+                general: [], metadata: [], ambiguous: [] }
+        ),
+        status: i.status,
+        rating: i.xml_rating,
+        source: i.source,
+        relationships: {
+            parentId: i.parent_id || null,
+            hasChildren: i.has_children
+        },
+        uploader: {
+            username: i.owner,
+            id: i.creator_id
+        },
+        hasNotes: i.has_notes,
+        commentCount: i.comment_count
+    }))
+});
