137eea143d7d54f71222245de9e77b03714e67b7
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-04-15 09:50:33 -0500

    feat: add default version

--- src/version-router.ts
+++ src/version-router.ts
@@ -3,6 +3,7 @@ import express from "express";
 import version0 from "./v0/router.ts";
 
 const versionRouter = express.Router()
+    .use("/", version0)
     .use("/v0", version0);
 
 export default versionRouter;
