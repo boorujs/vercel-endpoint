a1001ee31862e88369631fc8811469842fa7a813
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-04-16 12:44:46 -0500

    feat: add search functionality

--- src/v0/submodule/rule34/routing/search/router.ts
+++ src/v0/submodule/rule34/routing/search/router.ts
@@ -1,22 +1,35 @@
 import express from "express";
 import { formatResults } from "./util/format-results.ts";
+import { apiUrl } from "../../api/url/api-url.ts";
 import { fetchAndParse } from "../../../../util/parse.ts";
 
 import { authenticate } from "../../util/authenticate.ts";
 
 // temp
+import { AUTH } from "../../temp/auth.ts";
 const jsonUrl = "https://github.com/boorujs/booru.js/raw/refs/tags/v0.0.1/.brainstorm/rule34.xxx/responses/formatted/search.json";
 const xmlUrl =  "https://github.com/boorujs/booru.js/raw/refs/tags/v0.0.1/.brainstorm/rule34.xxx/responses/formatted/search.xml";
 
 const searchRouter = express.Router()
     // .use(authenticate)
     .get("/", async function (req, res) {
-        const json = await fetchAndParse.json(jsonUrl);
-        const xml = await fetchAndParse.xml(xmlUrl);
+        const query = {
+            user_id: AUTH.user_id,
+            api_key: AUTH.api_key,
+            tags: req.query.q,
+            limit: req.query.limit || 42,
+            page: req.query.page || 0
+        };
+
+        const json = await fetchAndParse.json(apiUrl("post", {
+            ...query,
+            json: 1,
+            fields: "tag_info"
+        }));
+        const xml = await fetchAndParse.xml(query);
 
         res.json({
             success: true,
-            $comment: "This is an example search. Functionality isn't available yet.",
             ...formatResults(json, xml)
         });
     });
