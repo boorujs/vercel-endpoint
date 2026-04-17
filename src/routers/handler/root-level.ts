d67ec9831365193ba218bbaa1f181de94bb4f6dc
Hazel <141371172+chiptumor@users.noreply.github.com>
2026-04-17 13:21:37 -0500

    fix: always redirect root level

--- src/routers/handler/root-level.ts
+++ src/routers/handler/root-level.ts
@@ -1,9 +1,5 @@
 const rootLevel = () => function rootLevel(req, res, next) {
-    const embeddingClientRegex =
-        /(discordbot|telegrambot|facebook|whatsapp|firefox\/92|vkshare|revoltchat|preview|iframely)/gi;
-    if (req.get("User-Agent")?.match(embeddingClientRegex))
-        res.redirect("https://github.com/boorujs/vercel-endpoint");
-    else next();
+    res.redirect("https://github.com/boorujs/vercel-endpoint");
 }
 
 export default rootLevel;
