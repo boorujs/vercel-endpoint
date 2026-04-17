6a80f88e41657eeb0ad1c237e0af128c92992d65
Belle (Laptop) <nightofthebuttlickers@gmail.com>
2026-04-16 20:16:11 -0500

    fix: add second arg to .reduce()

--- src/v0/submodule/rule34/routing/embed/util/format-string.ts
+++ src/v0/submodule/rule34/routing/embed/util/format-string.ts
@@ -1,4 +1,4 @@
 export const formatString = (string: string, object: any): string =>
     string.replace(/\{\s*([\w.]+)\s*\}/g, (match, path) =>
-        path.split(".").reduce((o, k) => o?.[k]) ?? match
+        path.split(".").reduce((o, k) => o?.[k], object) ?? match
     );
